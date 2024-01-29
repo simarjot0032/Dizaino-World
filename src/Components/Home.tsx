b84326898a25218b875818ae8e025caea6d12751
simarjot <simarjot0032@gmail.com>
2024-01-29 22:27:57 +0530

    update

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -6,7 +6,16 @@ export default function Home() {
     <>
       <Navbar />
       <div className="home-container">
-        <div className="home-hero-section"></div>
+        <div className="home-hero-section">
+          <div className="hero-heading-container">
+            <h1 className="hero-section-main-heading">
+              Your Space, Your Story
+            </h1>
+            <h2 className="hero-section-secondary-heading">
+              Let Dizaino World Write the Perfect Chapter in Design.
+            </h2>
+          </div>
+        </div>
       </div>
     </>
   );
