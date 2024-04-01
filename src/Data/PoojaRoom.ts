ecde324a75418e74683386cd898424d547e37099
simarjot <simarjot0032@gmail.com>
2024-04-02 01:03:08 +0530

    added some content in the Pooja roomm

---
+++ src/Data/PoojaRoom.ts
@@ -0,0 +1,35 @@
+export const AccordianPoojaRoom = [
+  {
+    question: "What services do you offer for pooja room design?",
+    answer:
+      "Our pooja room design services encompass a range of offerings, including consultation, conceptualization, customization, and installation. We specialize in creating sacred spaces that inspire devotion and tranquility.",
+  },
+  {
+    question: "How do you approach pooja room design projects?",
+    answer:
+      "Our approach to pooja room design is rooted in understanding the spiritual significance and cultural preferences of our clients. We collaborate closely with you to design a sacred space that reflects your beliefs, rituals, and personal style.",
+  },
+  {
+    question:
+      "Can you help me visualize my pooja room design before starting the project?",
+    answer:
+      "Yes, we leverage advanced design tools and techniques to provide you with visual representations of your pooja room design concept. This includes 3D renderings, sketches, and mood boards to ensure that the final result aligns with your vision.",
+  },
+  {
+    question: "Do you offer customizable design packages for pooja rooms?",
+    answer:
+      "Certainly! We understand that every pooja room is unique, and we offer customizable design packages tailored to your specific requirements and preferences. Whether you seek a traditional, contemporary, or fusion design, our packages can be adapted to suit your needs.",
+  },
+  {
+    question:
+      "What materials and finishes do you recommend for pooja room interiors?",
+    answer:
+      "We recommend materials and finishes that evoke a sense of serenity, sanctity, and durability. From premium woods and marble to intricate carvings and sacred symbols, we curate elements that enhance the spiritual ambiance of your pooja room.",
+  },
+  {
+    question:
+      "How long does a typical pooja room design project take from start to finish?",
+    answer:
+      "The timeline for a pooja room design project varies depending on factors such as size, complexity, and customization requirements. During the initial consultation, we'll provide you with a detailed project timeline based on your specific needs and preferences.",
+  },
+];
