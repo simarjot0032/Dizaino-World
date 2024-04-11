229c311021ec7f2ed77174d332d8d26e4dae1920
simarjot <simarjot0032@gmail.com>
2024-04-11 15:27:21 +0530

    Admin navbar complete

--- src/Components/ADMIN/AdminNavbar.tsx
+++ src/Components/ADMIN/AdminNavbar.tsx
@@ -2,6 +2,7 @@ import "@Styles/Admin/AdminNavbar.css";
 import { useState } from "react";
 import Logo from "/icon.png";
 import { GrMenu } from "react-icons/gr";
+import { Link } from "react-router-dom";
 export default function AdminNavbar() {
   const [navscroll, setnavscroll] = useState(false);
   const Handlescroll = (): any => {
@@ -24,6 +25,22 @@ export default function AdminNavbar() {
         </div>
         <div className="admin-navbar-right-container">
           <GrMenu size={42} className="admin-menu-icon" color="white" />
+          <div className="menu-container">
+            <div className="admin-menu-widgets">
+              <h2 className="admin-menu-heading">Admin Menu</h2>
+            </div>
+            <div className="admin-menu">
+              <Link to={"/Admin"} className="linktag-admin">
+                <li className="link-admin">Admin Home</li>
+              </Link>
+              <Link to={"/Contact Form"} className="linktag-admin">
+                <li className="link-admin">Contact Form </li>
+              </Link>
+              <Link to={"/ProjectCRUD"} className="linktag-admin">
+                <li className="link-admin">Project</li>
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
